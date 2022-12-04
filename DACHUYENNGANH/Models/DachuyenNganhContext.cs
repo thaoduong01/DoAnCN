311c90e30c06979daf1acd3b7406b38b98428849
camthuy1002 <104145861+camthuy1002@users.noreply.github.com>
2022-12-04 13:50:57 +0700

    Update last

--- DACHUYENNGANH/Models/DachuyenNganhContext.cs
+++ DACHUYENNGANH/Models/DachuyenNganhContext.cs
@@ -36,7 +36,7 @@ namespace DACHUYENNGANH.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+
                 optionsBuilder.UseSqlServer("Server=ADMIN\\CAMTHUY;Database=DAChuyenNganh;User Id=sa;password=admin12345;Trusted_Connection=False;MultipleActiveResultSets=true;");
             }
         }
@@ -372,7 +372,7 @@ namespace DACHUYENNGANH.Models
                     .IsUnicode(false);
 
                 entity.Property(e => e.IdKhachHangCaNhan)
-                    .HasMaxLength(10)
+                    .HasMaxLength(12)
                     .IsUnicode(false);
 
                 entity.Property(e => e.IdNhanVien)
@@ -459,7 +459,7 @@ namespace DACHUYENNGANH.Models
                 entity.ToTable("KhachHangCaNhan");
 
                 entity.Property(e => e.IdKhachHangCaNhan)
-                    .HasMaxLength(10)
+                    .HasMaxLength(12)
                     .IsUnicode(false);
 
                 entity.Property(e => e.CmndCccd)
